0f7f628ed3035cb0b03cfa1743d032b659d73bbd
ritikaj23 <ritikaj@skillup.online>
2024-11-29 05:10:07 -0500

    modified requirements.txt

--- lib/country_list.dart
+++ lib/country_list.dart
@@ -1,19 +1,18 @@
-// country_list.dart
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 Future<List<String>> fetchCountries() async {
-  List<String> countries = [
-    'United States',
-    'Canada',
-    'United Kingdom',
-    'Australia',
-    'India',
-    'Germany',
-    'France',
-    'Japan',
-    'China',
-    'Brazil',
-    'South Africa'
-  ];
+  final response =
+      await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
 
-  return countries;
+  if (response.statusCode == 200) {
+
+    List<dynamic> countriesJson = json.decode(response.body);
+    List<String> countryList = countriesJson
+        .map((country) => country['name']['common'] as String)
+        .toList();
+    return countryList;
+  } else {
+    throw Exception('Failed to load countries');
+  }
 }
